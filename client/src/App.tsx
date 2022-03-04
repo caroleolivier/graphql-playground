4233ecd43c8415785a3318e02b09784e54af5fb5
Carole Olivier <carole@bulb.co.uk>
2022-03-04 16:21:59 +0000

    chore: add react app

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>I am the header</header>
+      <p>Hello World</p>
+    </div>
+  );
+}
+
+export default App;
